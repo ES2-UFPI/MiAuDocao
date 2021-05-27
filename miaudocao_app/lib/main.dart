163d69bf5a4f790b8ae102c2ddcae2e290e097af
Hernandes Erick <erickhernandes4@gmail.com>
2021-05-26 21:46:24 -0300

    Ajustes para testar a tela de animal #11

--- miaudocao_app/lib/main.dart
+++ miaudocao_app/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:miaudocao_app/widgets/custom_textfield.dart';
 import './utils/app_routes.dart';
 import './views/cadastrar_animal_screen.dart';
+import 'utils/app_routes.dart';
+import 'utils/app_routes.dart';
+import 'utils/app_routes.dart';
+import 'views/visualizar_animal_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,46 +17,61 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
-        accentColor: Colors.amberAccent,
-        fontFamily: 'WorkSans'
-      ),
+          primarySwatch: Colors.amber,
+          accentColor: Colors.amberAccent,
+          fontFamily: 'WorkSans'),
       routes: {
         AppRoutes.HOME: (ctx) => MyHomePage(),
         AppRoutes.CADASTRAR_ANIMAL: (ctx) => CadastrarAnimalScreen()
-      }
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == AppRoutes.VISUALIZAR_ANIMAL) {
+          final String argument = settings.arguments;
+
+          return MaterialPageRoute(builder: (context) {
+            return VisualizarAnimalScreen(argument);
+          });
+        }
+
+        return null;
+      },
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final _idAnimalController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'MiAuDoção',
-          style: TextStyle(
-            fontWeight: FontWeight.w700
-          ),
+          style: TextStyle(fontWeight: FontWeight.w700),
         ),
       ),
-      body: Text(
-        'Este é o início do MiAuDoção',
-        style: TextStyle(
-          fontFamily: 'WorkSans',
-          fontWeight: FontWeight.w500,
-          fontSize: 16
-        )
+      body: Column(
+        children: [
+          CustomTextField(
+            hint: "Digite um ID válido",
+            maxLength: 20,
+            controller: _idAnimalController,
+            showCounter: true,
+          ),
+          ElevatedButton(
+              onPressed: () => Navigator.of(context).pushNamed(
+                  AppRoutes.VISUALIZAR_ANIMAL,
+                  arguments: _idAnimalController.text.toString()),
+              child: Text('Visualizar animal'))
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: Row(
-          children: [
-            Icon(Icons.add),
-            Text('Cadastrar animal')
-          ],
+          children: [Icon(Icons.add), Text('Cadastrar animal')],
         ),
-        onPressed: () => Navigator.of(context).pushNamed(AppRoutes.CADASTRAR_ANIMAL),
+        onPressed: () =>
+            Navigator.of(context).pushNamed(AppRoutes.CADASTRAR_ANIMAL),
       ),
     );
   }
