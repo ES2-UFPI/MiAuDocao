953290fee647c481b54a06da89b4b2ea561fb927
Diego Winter <diego@diegowinter.dev>
2021-06-18 16:25:55 -0300

    Incluída rota de cadastrar usuário

--- miaudocao_app/lib/utils/app_routes.dart
+++ miaudocao_app/lib/utils/app_routes.dart
@@ -2,4 +2,5 @@ class AppRoutes {
   static const HOME = '/';
   static const CADASTRAR_ANIMAL = '/cadastrar-animal';
   static const VISUALIZAR_ANIMAL = '/visualizar-animal';
+  static const CADASTRAR_USUARIO = '/cadastrar-usuario';
 }
