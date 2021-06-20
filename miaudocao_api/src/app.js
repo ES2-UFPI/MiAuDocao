c074709e8114db6a22ec2b52592160615fc667a0
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2021-06-20 20:29:27 -0300

    Implementada endpoint de manifestar interesse

--- miaudocao_api/src/app.js
+++ miaudocao_api/src/app.js
@@ -7,6 +7,7 @@ const index = require('./routes/index');
 const animalRoute = require('./routes/animalRoute');
 const buscaRoute = require('./routes/buscaRoute');
 const usuarioRoute = require('./routes/usuarioRoute');
+const manifestarInteresseRoute = require('./routes/manifestarInteresseRoute');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
@@ -23,5 +24,6 @@ app.use('/', index);
 app.use('/animais', animalRoute);
 app.use('/busca', buscaRoute);
 app.use('/usuario', usuarioRoute);
+app.use('/manifestar_interesse', manifestarInteresseRoute);
 
 module.exports = app;
