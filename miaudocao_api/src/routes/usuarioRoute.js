147050a9902e51937b8965df5b2ea348a5ffda61
Hernandes Erick <erickhernandes4@gmail.com>
2021-06-20 22:35:06 -0300

    Implementado endpoint para retornar animais que um usuario tem interesse

--- miaudocao_api/src/routes/usuarioRoute.js
+++ miaudocao_api/src/routes/usuarioRoute.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/usuarioController');
 router.post('/', controller.post);
 router.get('/', controller.get);
 router.get('/animais/', controller.getAnimais);
+router.get('/tenho_interesse/', controller.getTenhoInteresse);
 
 module.exports = router;
