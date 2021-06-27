08ad0db9a87146fa2afd767a4110788c69e43f88
Diego Winter <diego@diegowinter.dev>
2021-06-27 05:05:26 -0300

    Adicionados testes para o endpoint animais (POST) #56

--- miaudocao_api/__tests__/animal.test.js
+++ miaudocao_api/__tests__/animal.test.js
@@ -1,57 +1,60 @@
+const { nanoid } = require('nanoid');
 const request = require('supertest');
 const app = require('../src/app');
-const pool = require('../src/configs/db');
-const { createUser, deleteAll } = require('../src/utils/dbTestUtils');
+const { createUser, createAnimal, deleteAll, disableForeignCheck } = require('../src/utils/dbTestUtils');
 
 describe('Animal POST', () => {
-  // afterAll(async () => {
-  //   pool.end();
-  // });
-
-  /*it('should register an animal successfully', async () => {
-    const id = await createUser(); // criando um usuário e recebendo de volta o id que foi dado a ele
-
-    const res = await request(app).post('/animais')
+  it('should register an animal successfully', async () => {
+    const res1 = await request(app).post('/usuario')
       .send({
-        'user_id': id, // mesmo id do usuário recém criado
-        'nome': 'Animal Teste',
-        'descricao': 'Uma descrição do animal',
+        'nome': 'Usuario',
+        'foto': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/h/kg8ABKEB16zW65EAAAAASUVORK5CYII=',
+        'email': nanoid(20),
+        'telefone': '1234',
+        'password': '1234',
+        'pref_especie': 'gato',
+        'pref_porte': 'pequeno',
+        'pref_sexo': 'macho',
+        'pref_faixa_etaria': 'jovem',
+        'pref_raio_busca': 2
+      });
+
+    const userId = res1.body['id'];
+
+    const res2 = await request(app).post('/animais')
+      .send({
+        'user_id': userId,
+        'nome': 'Animal',
+        'descricao': 'Um animal',
         'especie': 'gato',
         'porte': 'pequeno',
-        'sexo': 'fêmea',
-        'faixa_etaria': 'filhote',
+        'sexo': 'macho',
+        'faixa_etaria': 'jovem',
         'endereco': 'rua x',
-        'latitude': '-5.021160',
-        'longitude': '-42.781343',
+        'latitude': '10',
+        'longitude': '10',
         'foto': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/h/kg8ABKEB16zW65EAAAAASUVORK5CYII='
       });
 
-      expect(res.status).toBe(201);
-  });*/
+      expect(res2.status).toBe(201);
+  });
 
-  // it('should refuse an animal register due to inexistent user', async () => {
-  //   await createUser(); // cria um usuário qualquer
+  it('should register an animal successfully', async () => {
+    const res1 = await request(app).post('/animais')
+      .send({
+        'user_id': 'id_inexistente',
+        'nome': 'Animal',
+        'descricao': 'Um animal',
+        'especie': 'gato',
+        'porte': 'pequeno',
+        'sexo': 'macho',
+        'faixa_etaria': 'jovem',
+        'endereco': 'rua x',
+        'latitude': '10',
+        'longitude': '10',
+        'foto': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/h/kg8ABKEB16zW65EAAAAASUVORK5CYII='
+      });
 
-  //   const res = await request(app).post('/animais')
-  //     .send({
-  //       'user_id': 'outroUsuario', // este usuário certamente é diferente
-  //       'nome': 'Animal Teste',
-  //       'descricao': 'Uma descrição do animal',
-  //       'especie': 'gato',
-  //       'porte': 'pequeno',
-  //       'sexo': 'fêmea',
-  //       'faixa_etaria': 'filhote',
-  //       'endereco': 'rua x',
-  //       'latitude': '-5.021160',
-  //       'longitude': '-42.781343',
-  //       'foto': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/h/kg8ABKEB16zW65EAAAAASUVORK5CYII='
-  //     });
-
-  //     expect(res.status).toBe(404);
-  // });
-  describe('Nome do teste', () => {
-    it('deve testar que true === true', () => {
-      expect(true).toBe(true);
-    });
+      expect(res1.status).toBe(404);
   });
 });
