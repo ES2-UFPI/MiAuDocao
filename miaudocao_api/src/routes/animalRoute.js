68aad0eace1969944b055aa93dc3fa763354b397
Diego Winter <diego@diegowinter.dev>
2021-07-05 17:44:35 -0300

    Adicionada rota para perguntas #60

--- miaudocao_api/src/routes/animalRoute.js
+++ miaudocao_api/src/routes/animalRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/animalController');
+const controller = require('../controllers/animalController');
 
 router.post('/', controller.post);
 router.get('/', controller.get);
 router.get('/interessados/', controller.getInteressados);
 router.put('/marcar_adotado/', controller.marcarAdotado);
+router.post('/:id/pergunta/', perguntaController.post);
+router.get('/:id/pergunta/all', perguntaController.getAll);
+router.get('/:id/pergunta/:id_pergunta', perguntaController.get);
+router.put('/:id/pergunta/:id_pergunta/responder', perguntaController.put);
 
 module.exports = router;
