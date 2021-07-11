270521783ebf1bcf83c9dab689bf9bd6c1c4f75b
Leandro Oliveira <oliveira.leandro826@gmail.com>
2021-07-11 14:30:14 -0300

    Implementado endpoint para adicionar um favorito #71

---
+++ miaudocao_api/src/controllers/favoritoController.js
@@ -0,0 +1,93 @@
+const { nanoid } = require('nanoid');
+const pool = require('../configs/db');
+
+exports.post = (req, res, next) => {
+  const id = nanoid(20);
+  const userId = req.body.user_id;
+  const animalId = req.body.animal_id;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send({
+        type: 'Database error',
+        description: 'Something went wrong. Try again.'
+      });
+
+      return;
+    }
+
+    connection.query(`SELECT * FROM usuario WHERE id = ?`, [
+      userId
+    ], function (error, results) {
+      if (error) {
+        connection.release();
+        res.status(400).send({
+          type: 'Database error',
+          description: 'One or more values are invalid (validating user).'
+        });
+      } else if (results[0]) {
+        connection.query(`SELECT * FROM animal WHERE id = ?`, [
+          animalId
+        ], function (error, results) {
+          if (error) {
+            connection.release();
+            res.status(400).send({
+              type: 'Database error',
+              description: 'One or more values are invalid (validating user).'
+            });
+          } else if (results[0]) {
+            connection.query(`SELECT * FROM favorito WHERE usuario_id = ? AND animal_id = ?`, [
+              userId,
+              animalId
+            ], function (error, results) {
+              if (error) {
+                connection.release();
+                res.status(400).send({
+                  type: 'Database error',
+                  description: 'One or more values are invalid (validating user).'
+                });
+              } else if (results[0]) {
+                res.status(400).send({
+                  type: 'Database error',
+                  description: 'This favorite entry already exists.'
+                });
+              } else {
+                connection.query(`INSERT INTO favorito VALUES (?, ?, ?)`, [
+                  id,
+                  userId,
+                  animalId
+                ], function (error) {
+                  connection.release();
+                  if (error) {
+                    res.status(400).send({
+                      type: 'Database error',
+                      description: 'One or more values are invalid (validating user).'
+                    });
+                  } else {
+                    res.status(201).send({
+                      type: 'Created',
+                      description: 'Favorite added successfully.',
+                      id: id
+                    });
+                  }
+                });
+              }
+            });
+          } else {
+            connection.release();
+            res.status(404).send({
+              type: 'Not found',
+              description: 'This animal does not exist.'
+            });
+          }
+        });
+      } else {
+        connection.release();
+        res.status(404).send({
+          type: 'Not found',
+          description: 'This user does not exist.'
+        });
+      }
+    });
+  });
+}
