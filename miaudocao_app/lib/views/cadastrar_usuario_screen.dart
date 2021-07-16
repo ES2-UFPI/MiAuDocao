b51d6242ea33c0601920cc0ef661000593c48d09
Maria Theresa <mariatheresa98@hotmail.com>
2021-07-16 00:18:11 -0300

    Implementada fachada de usuário

--- miaudocao_app/lib/views/cadastrar_usuario_screen.dart
+++ miaudocao_app/lib/views/cadastrar_usuario_screen.dart
@@ -1,15 +1,10 @@
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:miaudocao_app/models/usuario.dart';
-import 'package:miaudocao_app/utils/configs.dart';
+import 'package:miaudocao_app/models/usuario_facade.dart';
 import 'package:miaudocao_app/widgets/user_form.dart';
 
 class CadastrarUsuarioScreen extends StatelessWidget {
-  final Dio _dio = Dio();
-
   _showSnackBar(BuildContext context, String message) {
     final scaffold = ScaffoldMessenger.of(context);
     scaffold.showSnackBar(
@@ -39,38 +34,32 @@ class CadastrarUsuarioScreen extends StatelessWidget {
     );
   }
 
-  _submitRegister(Usuario usuario, BuildContext context) async {
+  void _submitRegister(Usuario usuario, BuildContext context) async {
     context.loaderOverlay.show();
-    try {
-      final response = await this._dio.post(
-        '${Configs.API_URL}/usuario', 
-        data: json.encode({
-          'nome': usuario.nome,
-          'foto': usuario.foto,
-          'email': usuario.email,
-          'telefone': usuario.telefone,
-          'password': usuario.password,
-          'pref_especie': usuario.prefEspecie,
-          'pref_porte': usuario.prefPorte,
-          'pref_sexo': usuario.prefSexo,
-          'pref_faixa_etaria': usuario.prefFaixaEtaria,
-          'pref_raio_busca': usuario.prefRaioBusca
-        })
-      );
+    bool registered = await UsuarioFacade.registerUser(
+      nome: usuario.nome,
+      foto: usuario.foto,
+      email: usuario.email,
+      telefone: usuario.telefone,
+      password: usuario.password,
+      prefEspecie: usuario.prefEspecie,
+      prefPorte: usuario.prefPorte,
+      prefSexo: usuario.prefSexo,
+      prefFaixaEtaria: usuario.prefFaixaEtaria,
+      prefRaioBusca: usuario.prefRaioBusca
+    );
 
-      context.loaderOverlay.hide();
-      if (response.statusCode == 201) {
-        _showSnackBar(context, 'Usuário cadastrado com sucesso! Agora você pode fazer o login.');
-        Navigator.of(context).pop();
-      }
-    } catch (e) {
-      print(e);
+    context.loaderOverlay.hide();
+    if (registered == true) {
+      _showSnackBar(context, 'Usuário cadastrado com sucesso! Agora você pode fazer o login.');
+      Navigator.of(context).pop();
+    } else {
       context.loaderOverlay.hide();
       Navigator.of(context).pop();
       _showAlertDialog(
         context,
         'Algo deu errado',
-        'Estamos com dificuldades de processar sua solicitação no momento. '
+        'Não conseguimos realizar o seu cadastro no momento. '
             + 'Você pode verificar os dados enviados ou tentar novamente mais '
             + 'tarde.'
       );
